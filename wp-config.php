851fbeb8539983e075b9f8365bdc45c091259b02
iietmoon <otmannj56@gmail.com>
2022-06-27 18:24:40 +0100

    onProd 🚀

--- wp-config.php
+++ wp-config.php
@@ -20,23 +20,30 @@
  */
 
 // ** Database settings - You can get this info from your web host ** //
+/*===========================================
+   onDev configs
+============================================*/
 /** The name of the database for WordPress */
+/*
 define( 'DB_NAME', 'local' );
-
-/** Database username */
 define( 'DB_USER', 'root' );
-
-/** Database password */
 define( 'DB_PASSWORD', 'root' );
-
-/** Database hostname */
 define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
-
-/** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+*/
+/*===========================================
+   onPev configs
+============================================*/
+// ** Database settings - You can get this info from your web host ** //
+
+define( 'DB_NAME', 'c1464837c_sengabs' );
+define( 'DB_USER', 'c1464837c_sengabs' );
+define( 'DB_PASSWORD', 'c{}])oSaZl1+' );
+define( 'DB_HOST', 'localhost' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
 
 /**#@+
  * Authentication unique keys and salts.
